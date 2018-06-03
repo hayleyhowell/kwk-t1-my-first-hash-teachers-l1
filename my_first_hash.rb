c7ab69a1d7bda0735c7cfb771897dfb59c9d6708
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-03 12:22:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   dictionary {
-    "fruit" => 
-    
+    "fruit" => "pineapple"
+    "vegetable" => "tomato"
+    "meat" => "beef"
   }
   
 end
